9bd671bf67fa67c293d0e4ce74dc056632b5f2cf
Ullrich Schäfer <ullrich@seidbereit.de>
2011-02-07 15:33:34 +0100

    Some changes for the streaming Mac framework target

--- Sources/SoundCloudAPI/SCSoundCloudAPI-Private.h
+++ Sources/SoundCloudAPI/SCSoundCloudAPI-Private.h
@@ -6,7 +6,11 @@
 //  Copyright 2011 nxtbgthng. All rights reserved.
 //
 
+#if TARGET_OS_IPHONE
 #import "SCSoundCloudAPI.h"
+#else
+#import <SoundCloudAPI/SCSoundCloudAPI.h>
+#endif
 
 
 @class SCSoundCloudAPIAuthentication;
